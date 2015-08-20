5c4f566a7ab86a1803a227d7d944c20279d2f45b
zoejiang2015 <z.jscjy@163.com>
2015-08-20 14:20:12 +0800

    by Zoe

---
+++ src/FordTest/ChromeIEDriver.java
@@ -0,0 +1,6 @@
+package FordTest;
+
+public class ChromeIEDriver
+{
+
+}
---
+++ src/FordTest/RedesignFordDealer.java
@@ -0,0 +1,39 @@
+package FordTest;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+//firefox driver
+
+public class RedesignFordDealer
+{
+	WebDriver driver;
+
+	@BeforeClass
+	public void setUp()
+	{
+		driver = new FirefoxDriver();
+		driver.get("http://wwwqa.ford.com/dealerships/locate?gnav=header-finddealer");
+		driver.manage().window().maximize();
+	}
+
+	@Test
+	public void testFindDealer()
+	{
+		driver.findElement(By.linkText("Close")).click();
+	}
+	
+	
+	@AfterClass
+	public void tearDown()
+	{
+		driver.quit();
+	}
+	
+	
+}
